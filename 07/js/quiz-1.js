c9d20eae9c0304f50edc911c02cfd0d6f6d2956a
kwon-o <koj1228853@gmail.com>
2021-03-02 21:58:39 +0900

    2021/03/02

--- 07/js/quiz-1.js
+++ 07/js/quiz-1.js
@@ -1,11 +1,23 @@
 		var numbers = [2, 4, 6, 8, 10];
 		showArray(numbers);
 
+		var addBtn = document.querySelector("#add");
+		addBtn.addEventListener("click", plusList)
+
+		function plusList() {
+			var sum = 0;
+			for(var i=0; i<numbers.length; i++) {
+				sum += numbers[i];
+			}
+			numbers.push(sum);
+			showArray(numbers);
+		}
+		
 		function showArray(arr) {
 			var str = "<table><tr>";
 			for (var i=0; i<arr.length; i++) {
 				str += "<td>" + arr[i] + "</td>";
 			}
 			str += "</tr></table>";
-			document.write(str);
+			document.querySelector("#printList").innerHTML = str;
 		}
